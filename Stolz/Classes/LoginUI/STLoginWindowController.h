f4a5857b7006fad104ec0a6b840e0ea01a361086
Moshe <moshberm@gmail.com>
2013-10-25 00:54:40 -0400

    Added a barebones window controller to the repository.

---
+++ Stolz/Classes/LoginUI/STLoginWindowController.h
@@ -0,0 +1,13 @@
+//
+//  STLoginWindowController.h
+//  Stolz
+//
+//  Created by Moshe Berman on 10/25/13.
+//  Copyright (c) 2013 Moshe Berman. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface STLoginWindowController : NSWindowController
+
+@end
---
+++ Stolz/Classes/LoginUI/STLoginWindowController.m
@@ -0,0 +1,33 @@
+//
+//  STLoginWindowController.m
+//  Stolz
+//
+//  Created by Moshe Berman on 10/25/13.
+//  Copyright (c) 2013 Moshe Berman. All rights reserved.
+//
+
+#import "STLoginWindowController.h"
+
+@interface STLoginWindowController ()
+
+@end
+
+@implementation STLoginWindowController
+
+- (id)initWithWindow:(NSWindow *)window
+{
+    self = [super initWithWindow:window];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+- (void)windowDidLoad
+{
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
