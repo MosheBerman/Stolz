3134218edb6fac1b77a25d3813f07055e17aa70b
Moshe <moshberm@gmail.com>
2013-10-25 04:39:08 -0400

    Added a header for external files to access.

---
+++ Stolz/StolzKit.h
@@ -0,0 +1,14 @@
+//
+//  StolzKit.h
+//  Stolz
+//
+//  Created by Moshe Berman on 10/25/13.
+//  Copyright (c) 2013 Moshe Berman. All rights reserved.
+//
+
+#ifndef Stolz_StolzKit_h
+#define Stolz_StolzKit_h
+
+#import "STLoginDirector.h"
+
+#endif
