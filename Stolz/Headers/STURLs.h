88b426acf4054a27bf5a28653059a2edaef1aea1
Moshe <moshberm@gmail.com>
2013-10-25 03:50:40 -0400

    Added some more URLs to the URLs header.

--- Stolz/Headers/STURLs.h
+++ Stolz/Headers/STURLs.h
@@ -11,6 +11,12 @@
 
 #define kRedirectURI @"https://www.facebook.com/connect/login_success.html"
 
-#define kLoginDialogURL @"https://www.facebook.com/dialog/oauth?client_id=%@&redirect_uri=%@"
+#define kLoginDialogURL @"https://www.facebook.com/dialog/oauth?client_id=%@&redirect_uri=%@&response_type=token&scope=read_stream,friends_online_presence,user_likes,user_groups"
+
+/**
+ *  {token-to-inspect}
+ *  {app-token-or-admin-token}
+ */
+#define kTokenInspectionURL @"graph.facebook.com/debug_token?input_token=%@&access_token=%@"
 
 #endif
