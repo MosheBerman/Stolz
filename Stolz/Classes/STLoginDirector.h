9deb9f0137ad082f831dc8db51b31206ce615583
Moshe <moshberm@gmail.com>
2013-10-25 00:28:51 -0400

    Began implementing the login director.

--- Stolz/Classes/STLoginDirector.h
+++ Stolz/Classes/STLoginDirector.h
@@ -8,11 +8,61 @@
 
 #import <Foundation/Foundation.h>
 
+#import "STLoginStates.h"
+#import "STHandlers.h"
+
 /**
  *  This class manages the login interaction for a 
- *  Facebook user in an OS X app.
+ *  Facebook user in an OS X app. As per the 
+ *  Facebook documentation, there are several
+ *  stages to handling login.
+ *  
+ *  The relevant page is here:
+ *  https://developers.facebook.com/docs/facebook-login/login-flow-for-web-no-jssdk/#checklogin
+ *
  */
 
 @interface STLoginDirector : NSObject
+
+/**
+ *  Checking login status
+ *
+ */
+
+/** 
+ *  Checks if the user is logged in. 
+ *  @return YES if logged in, else NO. 
+ */
+
+- (BOOL)isUserLoggedIn;
+
+/**
+ *  Checks if the user is authorized.
+ *
+ *  @discussion Facebook discusses the difference
+ *  between authorization and log in.
+ *
+ *  @return YES if the user is authorized, else NO.
+ */
+ 
+- (BOOL)isUserAuthorized;
+
+/**
+ *  Logging people in
+ */
+
+- (void)logUserInWithCompletion:(STLoginCompletionBlock)completion;
+
+/**
+ *  Logging people out
+ */
+
+- (void)logUserOutWithCompletion:(STLoginCompletionBlock)completion;
+
+/**
+ *  Facebook App ID
+ */
+
+- (void)setFacebookAppID:(NSString *)appID;
 
 @end
--- Stolz/Classes/STLoginDirector.m
+++ Stolz/Classes/STLoginDirector.m
@@ -8,8 +8,84 @@
 
 #import "STLoginDirector.h"
 
+#import "STTokenManager.h"
+
+#import "STURLs.h"
+
+@interface STLoginDirector ()
+
+/** Stores the user's login state. */
+@property (assign) enum STLoginState state;
+
+/** This token manager handles token management. */
+@property (strong) STTokenManager *tokenManager;
+
+/**
+ *  Allows the user to set the application ID. 
+ */
+@property (strong) NSString *facebookAppID;
+
+@end
+
 @implementation STLoginDirector
 
+/**
+ *  Checks if the user is logged in.
+ *  @return YES if logged in, else NO.
+ */
+
+- (BOOL)isUserLoggedIn
+{
+    return (_state == STLoginStateLoggedIn);
+}
+
+/**
+ *  Checks if the user is authorized.
+ *
+ *  @discussion Facebook discusses the difference
+ *  between authorization and log in.
+ *
+ *  @return YES if the user is authorized, else NO.
+ */
+
+- (BOOL)isUserAuthorized
+{
+    return (_state != STLoginStateLoggedNotAuthorized);
+}
+
+/**
+ *  Logging people in
+ */
+
+- (void)logUserInWithCompletion:(STLoginCompletionBlock)completion
+{
+    
+    NSString *loginString = [NSString stringWithFormat:kLoginDialogURL, self.facebookAppID, kRedirectURI];
+    NSURL *loginURL = [NSURL URLWithString:loginString];
+    [[NSWorkspace sharedWorkspace] openURL:loginURL];
+}
+
+/**
+ *  Logging people out
+ */
+
+- (void)logUserOutWithCompletion:(STLoginCompletionBlock)completion
+{
+    
+}
+
+/**
+ *  Confirming Identity
+ */
+
+- (void)confirmUserIdentity
+{
+    
+}
+
+/**
+ *  Storing access tokens and login status
+ */
 
 
 @end
