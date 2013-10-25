f2752363c90ab8b5aafb65c62216d5a344b50577
Moshe <moshberm@gmail.com>
2013-10-25 00:50:01 -0400

    Added the handlers header to the project.

---
+++ Stolz/Headers/STHandlers.h
@@ -0,0 +1,16 @@
+//
+//  Handlers.h
+//  Stolz
+//
+//  Created by Moshe Berman on 10/25/13.
+//  Copyright (c) 2013 Moshe Berman. All rights reserved.
+//
+
+#import "STLoginStates.h"
+
+#ifndef Stolz_STHandlers_h
+#define Stolz_STHandlers_h
+
+typedef void(^STLoginCompletionBlock)(BOOL success, enum STLoginState state);
+
+#endif
