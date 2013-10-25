727e6b0edaffe0391cb494a51ced82685b2076ce
Moshe <moshberm@gmail.com>
2013-10-25 02:01:47 -0400

    Added an application constants header for testing.

---
+++ Stolz/Headers/STApplicationConstants.h
@@ -0,0 +1,14 @@
+//
+//  ApplicationConstants.h
+//  Stolz
+//
+//  Created by Moshe Berman on 10/25/13.
+//  Copyright (c) 2013 Moshe Berman. All rights reserved.
+//
+
+#ifndef Stolz_STApplicationConstants_h
+#define Stolz_STApplicationConstants_h
+
+#define kFacebookApplicationID @""
+
+#endif
