0e9f52431307d96361014a63530cdc7672b3329f
Moshe <moshberm@gmail.com>
2013-10-24 23:52:40 -0400

    Added an empty header for URLs.

---
+++ Stolz/Headers/STURLs.h
@@ -0,0 +1,14 @@
+//
+//  STURLs.h
+//  Stolz
+//
+//  Created by Moshe Berman on 10/24/13.
+//  Copyright (c) 2013 Moshe Berman. All rights reserved.
+//
+
+#ifndef Stolz_STURLs_h
+#define Stolz_STURLs_h
+
+
+
+#endif
