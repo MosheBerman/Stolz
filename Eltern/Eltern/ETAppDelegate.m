105a6d3fccfca1d0074ff726630b27b9ab674b70
Moshe <moshberm@gmail.com>
2013-10-26 21:08:39 -0400

    Began building out demo some more.

--- Eltern/Eltern/ETAppDelegate.m
+++ Eltern/Eltern/ETAppDelegate.m
@@ -12,6 +12,8 @@
 @interface ETAppDelegate ()
 
 @property (strong) STLoginDirector *loginDirector;
+@property (weak) IBOutlet NSTextField *loginLabel;
+@property (weak) IBOutlet NSButton *loginButton;
 
 @end
 
@@ -23,11 +25,49 @@
     self.loginDirector = [[STLoginDirector alloc] init];
 }
 
-- (IBAction)logIn:(id)sender {
+
+- (IBAction)toggleLogin:(id)sender {
+
+    if(self.loginDirector.isUserLoggedIn)
+    {
+        [self logOut];
+    }
+    else {
+        [self logIn];
+    }
+
+}
+
+#pragma mark - Login/Logout
+
+/**
+ *
+ */
+
+- (void)logIn {
     [[self loginDirector] setFacebookAppID:@"191646724354232"];
     [self.loginDirector logUserInWithCompletion:^(BOOL success, enum STLoginState state) {
+        
+        if (success) {
+            self.loginLabel.stringValue = NSLocalizedString(@"You are logged in.", @"A label for reporting that a user is logged in, without the username.");
+            self.loginButton.title = NSLocalizedString(@"Log Out", @"A label for as button to log out.");
+            
+        }
+        else {
+            self.loginLabel.stringValue = NSLocalizedString(@"You are not logged in.", @"A label for reporting that a user is logged out.");
+            self.loginButton.title = NSLocalizedString(@"Log In", @"A label for as button to log in.");
+        }
         
     }];
 }
 
+/**
+ *
+ */
+
+- (void)logOut
+{
+    
+}
+ 
 @end
