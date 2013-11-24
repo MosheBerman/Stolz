8963c6d1f43671076ac1c5621dd8972cd4f39a58
Moshe <moshberm@gmail.com>
2013-11-24 15:27:20 -0500

    Added NSCoding to the STToken class.

--- Stolz/Classes/STToken.m
+++ Stolz/Classes/STToken.m
@@ -8,6 +8,38 @@
 
 #import "STToken.h"
 
+@interface STToken () <NSCoding>
+
+@end
+
 @implementation STToken
+
+#pragma mark - NSCoding
+
+- (id)initWithCoder:(NSCoder *)aDecoder {
+    self = [super init];
+    
+    if (self) {
+        
+        _expirationDate = [aDecoder decodeObjectForKey:@"expirationDate"];
+        _issueDate = [aDecoder decodeObjectForKey:@"issueDate"];
+        _machineID = [aDecoder decodeObjectForKey:@"machineID"];
+        _scopes = [aDecoder decodeObjectForKey:@"scopes"];
+        _metadata = [aDecoder decodeObjectForKey:@"metadata"];
+        _userID = [aDecoder decodeObjectForKey:@"userID"];
+        
+    }
+    return self;
+}
+
+- (void)encodeWithCoder:(NSCoder *)aCoder {
+    [aCoder encodeObject:self.expirationDate forKey:@"expirationDate"];
+    [aCoder encodeObject:self.issueDate forKey:@"issueDate"];
+    [aCoder encodeObject:self.machineID forKey:@"machineID"];
+    [aCoder encodeObject:self.scopes forKey:@"scopes"];
+    [aCoder encodeObject:self.metadata forKey:@"metadata"];
+    [aCoder encodeObject:self.userID forKey:@"userID"];
+}
+
 
 @end
