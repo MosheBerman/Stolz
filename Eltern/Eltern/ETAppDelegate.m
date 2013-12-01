4b2a6d85bb955a94bec5947a2de99d87a2076502
Moshe <moshberm@gmail.com>
2013-11-30 22:57:48 -0500

    Changed the demo to use the shared director.

--- Eltern/Eltern/ETAppDelegate.m
+++ Eltern/Eltern/ETAppDelegate.m
@@ -22,7 +22,7 @@
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     // Insert code here to initialize your application
-    self.loginDirector = [[STLoginDirector alloc] init];
+    self.loginDirector = [STLoginDirector sharedDirector];
 }
 
 
